1e21b20df870346899cc2820fb4ad8660d46e513
Lobsterrinho <lobsterrinho@gmail.com>
2023-02-26 23:22:17 +0300

    Commit init

---
+++ UBeer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UBeer
+//
+//  Created by Lobster on 26.02.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
